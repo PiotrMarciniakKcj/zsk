59dab8a90bb05b26b372fdd2b920356251cf5c4f
PiotrMarciniakKcj <90185291+PiotrMarciniakKcj@users.noreply.github.com>
2021-10-06 15:42:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+int main(){
+	setlocale(LC_CTYPE,"polish");
+	int x, y;
+	x=10;
+	y=x;
+	
+	int *p_test;
+	p_test=&x;
+	
+	cout<<p_test<<endl;  //0x6ffe14
+	
+	p_test=&y;
+	
+	cout<<p_test<<endl; //0x6ffe10
+	
+	
+
+	
+	return 0;
+}
